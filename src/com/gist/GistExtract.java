98850b9352200d4ad9b9a8c89198bbe0dc42f9f0
Vivek Mahadevan <vivek_veeramani@intuit.com>
2014-08-12 10:45:03 -0700

    Corrected Formatting

--- src/com/gist/GistExtract.java
+++ src/com/gist/GistExtract.java
@@ -16,78 +16,71 @@ import org.json.JSONObject;
 
 public class GistExtract {
 
-	
-	public static final  String GIT_URL ="https://api.github.com/users/KodeSeeker/gists";
-	public static final  String workSpacePath="/Users/KodeSeeker/Personal/git_repo/"; 
-	
+	public static final String GIT_URL = "https://api.github.com/users/KodeSeeker/gists";
+	public static final String WORKSPACE_PATH = "/Users/KodeSeeker/Personal/git_repo/";
+
 	private static String readAll(Reader rd) throws IOException {
-	    StringBuilder sb = new StringBuilder();
-	    int cp;
-	    while ((cp = rd.read()) != -1) {
-	      sb.append((char) cp);
-	    }
-	    return sb.toString();
-	  }
-	
-	public static void JSONExtract()
-	{
-	try
-	{
-	URL url = new URL(GIT_URL);
-	
-	 InputStream is = url.openStream();
-	 BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-     String jsonText = readAll(rd);
-     JSONArray jsArray = new JSONArray(jsonText); 
-   
-	
-	for(int i=0;i<jsArray.length();i++)
-	{
-		JSONObject jsOb= jsArray.getJSONObject(i);
-		JSONObject jsChildOb=(JSONObject)jsOb.get("files");
-
-		Iterator<String> iterator  = jsChildOb.keys();
-		String key = null;
-		while(iterator.hasNext()){
-            key = (String)iterator.next();
-          
-            String fname = (String)((JSONObject)jsChildOb.get(key)).get("filename");
-            FileWriter fw= new FileWriter(workSpacePath+fname);
-            URL rawURL= new URL((String)((JSONObject)jsChildOb.get(key)).get("raw_url"));
-            InputStream in= rawURL.openStream();
-            BufferedReader buff= new BufferedReader(new InputStreamReader(in, Charset.forName("UTF-8")));
-            String rawCode=readAll(buff);
-            System.out.println(rawCode);
-            fw.write(rawCode);
-            fw.close();
-   
-          
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
 		}
-		
+		return sb.toString();
 	}
-	}
-	
-	
-	catch(MalformedURLException mal)
-	{
-		mal.printStackTrace();
-	}
-	
-	catch(IOException io)
-	{
-		io.printStackTrace();
-	} 
-	catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
-	}
-	
 
-	public static void main (String... args)
-	{
+	public static void JSONExtract() {
+		try {
+			URL url = new URL(GIT_URL);
+
+			InputStream is = url.openStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+					Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONArray jsArray = new JSONArray(jsonText);
+
+			for (int i = 0; i < jsArray.length(); i++) {
+				JSONObject jsOb = jsArray.getJSONObject(i);
+				JSONObject jsChildOb = (JSONObject) jsOb.get("files");
+
+				Iterator<String> iterator = jsChildOb.keys();
+				String key = null;
+				while (iterator.hasNext()) {
+					key = (String) iterator.next();
+
+					String fname = (String) ((JSONObject) jsChildOb.get(key))
+							.get("filename");
+					FileWriter fw = new FileWriter(WORKSPACE_PATH + fname);
+					URL rawURL = new URL(
+							(String) ((JSONObject) jsChildOb.get(key))
+									.get("raw_url"));
+					InputStream in = rawURL.openStream();
+					BufferedReader buff = new BufferedReader(
+							new InputStreamReader(in, Charset.forName("UTF-8")));
+					String rawCode = readAll(buff);
+					System.out.println(rawCode);
+					fw.write(rawCode);
+					fw.close();
+
+				}
+
+			}
+		}
+
+		catch (MalformedURLException mal) {
+			mal.printStackTrace();
+		}
+
+		catch (IOException io) {
+			io.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String... args) {
 		JSONExtract();
 	}
-	
+
 }
